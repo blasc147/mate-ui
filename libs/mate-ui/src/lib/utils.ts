f1af199e76c5ff859faa9a2d7cbc6bac82a2031c
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-07 18:14:28 -0300

    chore: update tailwind-merge typography class groups

--- libs/mate-ui/src/lib/utils.ts
+++ libs/mate-ui/src/lib/utils.ts
@@ -1,5 +1,12 @@
 import { cx, type CxOptions } from 'class-variance-authority';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  classGroups: {
+    'font-size': [{ text: ['m'] }],
+    'font-weight': [{ font: ['regular'] }],
+  },
+});
 
 export function cn(...inputs: CxOptions) {
   return twMerge(cx(inputs));
