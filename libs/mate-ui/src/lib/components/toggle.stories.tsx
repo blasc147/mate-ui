f38c1b4156b2f5b59493220924d96ec92ea7810c
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-12 16:36:48 -0300

    feat(mate-ui): add toggle component (#9)

---
+++ libs/mate-ui/src/lib/components/toggle.stories.tsx
@@ -0,0 +1,105 @@
+import type { Meta } from '@storybook/react';
+import { Toggle } from './toggle';
+
+const Story: Meta<typeof Toggle> = {
+  component: ({ children }) => <>{children}</>,
+  title: 'Toggle',
+};
+export default Story;
+
+export const SimpleToggle = {
+  args: {
+    children: <Toggle />,
+  },
+};
+
+export const ToggleWithLabel = {
+  args: {
+    children: <Toggle label="Label" />,
+  },
+};
+
+export const ToggleWithLabelAndSubText = {
+  args: {
+    children: (
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+    ),
+  },
+};
+
+export const MultipleTogglesInline = {
+  args: {
+    children: (
+      <div className="flex gap-4 items-start">
+        <Toggle
+          label="Label"
+          subtext="Additional text to accompany the label"
+        />
+        <Toggle
+          label="Label"
+          subtext="Additional text to accompany the label"
+        />
+        <Toggle
+          label="Label"
+          subtext="Additional text to accompany the label"
+        />
+      </div>
+    ),
+  },
+};
+
+export const TrailingControl = {
+  args: {
+    children: (
+      <Toggle
+        label="Label"
+        subtext="Additional text to accompany the label"
+        trailingControl
+      />
+    ),
+  },
+};
+
+export const MultipleTogglesInlineTrailingControl = {
+  args: {
+    children: (
+      <div className="flex gap-5">
+        <Toggle
+          label="Label"
+          subtext="Additional text to accompany the label"
+          trailingControl
+        />
+        <Toggle
+          label="Label"
+          subtext="Additional text to accompany the label"
+          trailingControl
+        />
+      </div>
+    ),
+  },
+};
+
+export const Disabled = {
+  args: {
+    children: (
+      <Toggle
+        label="Label"
+        subtext="Additional text to accompany the label"
+        disabled
+      />
+    ),
+  },
+};
+
+export const DisabledChecked = {
+  args: {
+    children: (
+      <Toggle
+        label="Label"
+        subtext="Additional text to accompany the label"
+        disabled
+        checked
+      />
+    ),
+  },
+};
---
+++ libs/mate-ui/src/lib/components/toggle.tsx
@@ -0,0 +1,155 @@
+'use client';
+
+import * as SwitchPrimitives from '@radix-ui/react-switch';
+import * as Label from '@radix-ui/react-label';
+import * as React from 'react';
+import { cn } from '../utils';
+import { CheckIcon } from '@heroicons/react/24/outline';
+import { cva } from 'class-variance-authority';
+
+const styles = {
+  root: cva(['inline-grid', 'gap-x-2', 'items-center'], {
+    variants: {
+      trailingControl: {
+        true: 'grid-cols-[auto_40px]',
+        false: 'grid-cols-[40px_auto]',
+      },
+    },
+    defaultVariants: {
+      trailingControl: false,
+    },
+  }),
+  toggle: cn([
+    'inline-flex',
+    'h-5',
+    'w-10',
+    'shrink-0',
+    'cursor-pointer',
+    'items-center',
+    'rounded-full',
+    'border-2',
+    'border-transparent',
+    'transition-colors',
+    'enabled:hover:shadow-md',
+    'focus-visible:outline-none',
+    'focus-visible:ring',
+    'focus-visible:ring-focus',
+    'focus-visible:ring-offset-2',
+    'disabled:cursor-not-allowed',
+    'disabled:data-[disabled]:bg-neutral-300',
+    'data-[state=checked]:bg-primary-500',
+    'data-[state=unchecked]:bg-neutral-400',
+    'active:data-[state=checked]:bg-primary-700',
+    'active:data-[state=unchecked]:bg-neutral-600',
+    'hover:data-[state=unchecked]:bg-neutral-500',
+    'group',
+    'peer',
+  ]),
+  checkIcon: cn(
+    'pl-1',
+    'h-3.5',
+    'w-3.5',
+    'absolute',
+    'text-white',
+    'group-data-[disabled]:text-neutral-500',
+    'stroke-[4]'
+  ),
+  thumb: cn(
+    'pointer-events-none',
+    'block',
+    'h-4',
+    'w-4',
+    'rounded-full',
+    'shadow-lg',
+    'ring-0',
+    'transition-transform',
+    'bg-white',
+    'data-[disabled]:bg-neutral-500',
+    'data-[state=checked]:translate-x-5',
+    'data-[state=unchecked]:translate-x-0'
+  ),
+  label: cva(
+    [
+      'font-semibold',
+      'text-sm',
+      'text-neutral-900',
+      'peer-data-[disabled]:text-neutral-600',
+    ],
+    {
+      variants: {
+        trailingControl: {
+          true: 'order-first',
+        },
+      },
+    }
+  ),
+  subtext: cva(
+    [
+      'col-span-full',
+      'font-regular',
+      'text-sm',
+      'text-neutral-900',
+      'peer-data-[disabled]:text-neutral-600',
+    ],
+    {
+      variants: {
+        trailingControl: {
+          true: 'mr-[49px]',
+          false: 'ml-[49px]',
+        },
+      },
+      defaultVariants: {
+        trailingControl: false,
+      },
+    }
+  ),
+};
+
+interface ToggleProps
+  extends React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
+  label?: string;
+  subtext?: string;
+  trailingControl?: boolean;
+}
+
+const Toggle = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  ToggleProps
+>(({ className, label, subtext, trailingControl, id: _id, ...props }, ref) => {
+  const defaultId = React.useId();
+  const id = _id ?? defaultId;
+
+  return (
+    <div className={cn(styles.root({ trailingControl }), className)}>
+      <SwitchPrimitives.Root
+        className={styles.toggle}
+        id={id}
+        ref={ref}
+        {...props}
+      >
+        <CheckIcon className={styles.checkIcon} />
+
+        <SwitchPrimitives.Thumb className={styles.thumb} />
+      </SwitchPrimitives.Root>
+
+      {label && (
+        <Label.Root htmlFor={id} className={styles.label({ trailingControl })}>
+          {label}
+        </Label.Root>
+      )}
+
+      {subtext && (
+        <Label.Root
+          htmlFor={id}
+          className={styles.subtext({ trailingControl })}
+        >
+          {subtext}
+        </Label.Root>
+      )}
+    </div>
+  );
+});
+
+Toggle.displayName = 'Toggle';
+
+export { Toggle };
