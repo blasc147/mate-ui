d3a643d72d0b685b549617cf3cf12f36769e61c1
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-15 19:45:03 -0300

    refactor(mate-ui): header storybook

--- libs/mate-ui/src/lib/components/header.stories.tsx
+++ libs/mate-ui/src/lib/components/header.stories.tsx
@@ -1,164 +1,106 @@
-import type { Meta } from '@storybook/react';
-import {
-  Header,
-  type HeaderProps,
-  HeaderSupportiveText,
-  type HeaderSupportiveTextProps,
-} from './header';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Header, HeaderSupportiveText } from './header';
 
-interface CompProps {
-  headerProps: HeaderProps;
-  supportiveTextProps?: HeaderSupportiveTextProps;
-}
-
-const Comp = ({ headerProps, supportiveTextProps }: CompProps) => {
-  return (
-    <>
-      <Header {...headerProps} />
-      {supportiveTextProps?.children && (
-        <HeaderSupportiveText {...supportiveTextProps} />
-      )}
-    </>
-  );
-};
-
-const Story: Meta<typeof Comp> = {
-  component: Comp,
+const Story: Meta<typeof Header> = {
   title: 'Header',
 };
 
 export default Story;
 
-export const H1 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h1',
-    },
-  },
+type Story = StoryObj<typeof Header>;
+
+export const H1: Story = {
+  render: () => <Header variant="h1">Header</Header>,
+  name: 'H1',
 };
 
-export const H1WithSupportiveText = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h1',
-    },
-    supportiveTextProps: {
-      children: 'Supportive text',
-      variant: 'h1',
-    },
-  },
+export const H1WithSupportiveText: Story = {
+  render: () => (
+    <>
+      <Header variant="h1">Header</Header>
+      <HeaderSupportiveText variant="h1">Supportive text</HeaderSupportiveText>
+    </>
+  ),
+  name: 'H1 with Supportive Text',
 };
 
-export const H2 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h2',
-    },
-  },
+export const H2: Story = {
+  render: () => <Header variant="h2">Header</Header>,
+  name: 'H2',
 };
 
-export const H2WithSupportiveText = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h2',
-    },
-    supportiveTextProps: {
-      children: 'Supportive text',
-      variant: 'h2',
-    },
-  },
+export const H2WithSupportiveText: Story = {
+  render: () => (
+    <>
+      <Header variant="h2">Header</Header>
+      <HeaderSupportiveText variant="h2">Supportive text</HeaderSupportiveText>
+    </>
+  ),
+  name: 'H2 with Supportive Text',
 };
 
-export const H3 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h3',
-    },
-  },
+export const H3: Story = {
+  render: () => <Header variant="h3">Header</Header>,
+  name: 'H3',
 };
 
-export const H3WithSupportiveText = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h3',
-    },
-    supportiveTextProps: {
-      children: 'Supportive text',
-      variant: 'h3',
-    },
-  },
+export const H3WithSupportiveText: Story = {
+  render: () => (
+    <>
+      <Header variant="h3">Header</Header>
+      <HeaderSupportiveText variant="h3">Supportive text</HeaderSupportiveText>
+    </>
+  ),
+  name: 'H3 with Supportive Text',
 };
 
-export const H4 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h4',
-    },
-  },
+export const H4: Story = {
+  render: () => <Header variant="h4">Header</Header>,
+  name: 'H4',
 };
 
-export const H4WithSupportiveText = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h4',
-    },
-    supportiveTextProps: {
-      children: 'Supportive text',
-      variant: 'h4',
-    },
-  },
+export const H4WithSupportiveText: Story = {
+  render: () => (
+    <>
+      <Header variant="h4">Header</Header>
+      <HeaderSupportiveText variant="h4">Supportive text</HeaderSupportiveText>
+    </>
+  ),
+  name: 'H4 with Supportive Text',
 };
 
-export const H5 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h5',
-    },
-  },
+export const H5: Story = {
+  render: () => <Header variant="h5">Header</Header>,
+  name: 'H5',
 };
 
-export const H5WithSupportiveText = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h5',
-    },
-    supportiveTextProps: {
-      children: 'Supportive text',
-      variant: 'h5',
-    },
-  },
+export const H5WithSupportiveText: Story = {
+  render: () => (
+    <>
+      <Header variant="h5">Header</Header>
+      <HeaderSupportiveText variant="h5">Supportive text</HeaderSupportiveText>
+    </>
+  ),
+  name: 'H5 with Supportive Text',
 };
 
-export const H1AsH2 = {
-  args: {
-    headerProps: {
-      asChild: true,
-      children: <h2>Header</h2>,
-      variant: 'h1',
-    },
-  },
+export const H1AsH2: Story = {
+  render: () => (
+    <Header variant="h1" asChild>
+      <h2>Header</h2>
+    </Header>
+  ),
+  name: 'H1 as H2',
 };
 
-export const SupportiveTextAsH2 = {
-  args: {
-    headerProps: {
-      children: 'Header',
-      variant: 'h1',
-    },
-    supportiveTextProps: {
-      asChild: true,
-      children: <h2>Supportive text</h2>,
-      variant: 'h1',
-    },
-  },
+export const SupportiveTextAsH2: Story = {
+  render: () => (
+    <>
+      <Header variant="h1">Header</Header>
+      <HeaderSupportiveText variant="h1" asChild>
+        <h2>Supportive text</h2>
+      </HeaderSupportiveText>
+    </>
+  ),
+  name: 'Supportive Text as H2',
 };
