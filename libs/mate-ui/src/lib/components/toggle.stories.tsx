913cb566f8e600414bc49d5f207186d70ac5787f
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-15 18:36:25 -0300

    refactor(mate-ui): toggle storybook

--- libs/mate-ui/src/lib/components/toggle.stories.tsx
+++ libs/mate-ui/src/lib/components/toggle.stories.tsx
@@ -1,111 +1,96 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Toggle } from './toggle';
 
 const Story: Meta<typeof Toggle> = {
-  component: ({ children }) => <>{children}</>,
+  component: Toggle,
   title: 'Toggle',
 };
 export default Story;
 
-export const SimpleToggle = {
-  args: {
-    children: <Toggle />,
-  },
+type Story = StoryObj<typeof Toggle>;
+
+export const SimpleToggle: Story = {
+  render: () => <Toggle />,
 };
 
-export const DestructiveColorScheme = {
-  args: {
-    children: <Toggle colorScheme="destructive" />,
-  },
+export const DestructiveColorScheme: Story = {
+  render: () => <Toggle colorScheme="destructive" />,
 };
 
-export const ToggleWithLabel = {
-  args: {
-    children: <Toggle label="Label" />,
-  },
+export const ToggleWithLabel: Story = {
+  render: () => <Toggle label="Label" />,
 };
 
-export const ToggleWithLabelAndSubText = {
-  args: {
-    children: (
+export const ToggleWithLabelAndSubText: Story = {
+  render: () => (
+    <Toggle label="Label" subtext="Additional text to accompany the label" />
+  ),
+};
+
+export const MultipleTogglesInline: Story = {
+  render: () => (
+    <div className="flex gap-4">
       <Toggle label="Label" subtext="Additional text to accompany the label" />
-    ),
-  },
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+    </div>
+  ),
 };
 
-export const MultipleTogglesInline = {
-  args: {
-    children: (
-      <div className="flex gap-4 items-start">
-        <Toggle
-          label="Label"
-          subtext="Additional text to accompany the label"
-        />
-        <Toggle
-          label="Label"
-          subtext="Additional text to accompany the label"
-        />
-        <Toggle
-          label="Label"
-          subtext="Additional text to accompany the label"
-        />
-      </div>
-    ),
-  },
+export const MultipleTogglesInlineVertically: Story = {
+  render: () => (
+    <div className="flex flex-col gap-4">
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+      <Toggle label="Label" subtext="Additional text to accompany the label" />
+    </div>
+  ),
 };
 
-export const TrailingControl = {
-  args: {
-    children: (
+export const TrailingControl: Story = {
+  render: () => (
+    <Toggle
+      label="Label"
+      subtext="Additional text to accompany the label"
+      trailingControl
+    />
+  ),
+};
+
+export const MultipleTogglesInlineTrailingControl: Story = {
+  render: () => (
+    <div className="flex gap-5">
       <Toggle
         label="Label"
         subtext="Additional text to accompany the label"
         trailingControl
       />
-    ),
-  },
-};
-
-export const MultipleTogglesInlineTrailingControl = {
-  args: {
-    children: (
-      <div className="flex gap-5">
-        <Toggle
-          label="Label"
-          subtext="Additional text to accompany the label"
-          trailingControl
-        />
-        <Toggle
-          label="Label"
-          subtext="Additional text to accompany the label"
-          trailingControl
-        />
-      </div>
-    ),
-  },
-};
-
-export const Disabled = {
-  args: {
-    children: (
       <Toggle
         label="Label"
         subtext="Additional text to accompany the label"
-        disabled
+        trailingControl
       />
-    ),
-  },
+    </div>
+  ),
 };
 
-export const DisabledChecked = {
-  args: {
-    children: (
-      <Toggle
-        label="Label"
-        subtext="Additional text to accompany the label"
-        disabled
-        checked
-      />
-    ),
-  },
+export const Disabled: Story = {
+  render: () => (
+    <Toggle
+      label="Label"
+      subtext="Additional text to accompany the label"
+      disabled
+    />
+  ),
+};
+
+export const DisabledChecked: Story = {
+  render: () => (
+    <Toggle
+      label="Label"
+      subtext="Additional text to accompany the label"
+      disabled
+      checked
+    />
+  ),
 };
