bde6fa6a87d406c05e605e9c1e5d98f447c92d4d
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-15 20:23:24 -0300

    refactor(mate-ui): link storybook

--- libs/mate-ui/src/lib/components/link.stories.tsx
+++ libs/mate-ui/src/lib/components/link.stories.tsx
@@ -13,6 +13,9 @@ type Story = StoryObj<typeof Link>;
 export const Primary: Story = {
   args: {
     children: 'Link',
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
@@ -20,6 +23,9 @@ export const Secondary: Story = {
   args: {
     children: 'Link',
     colorScheme: 'secondary',
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
@@ -27,6 +33,9 @@ export const Neutral: Story = {
   args: {
     children: 'Link',
     colorScheme: 'neutral',
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
@@ -34,6 +43,9 @@ export const Destructive: Story = {
   args: {
     children: 'Link',
     colorScheme: 'destructive',
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
@@ -41,6 +53,9 @@ export const PrimaryWithLeftIcon: Story = {
   args: {
     children: 'Link',
     leftIcon: <BellIcon />,
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
@@ -48,6 +63,9 @@ export const PrimaryWithRightIcon: Story = {
   args: {
     children: 'Link',
     rightIcon: <BellIcon />,
+    href: 'https://www.google.com',
+    rel: 'noreferrer',
+    target: '_blank',
   },
 };
 
