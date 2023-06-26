accaf098a330ddc8e08e1be43058b5d2c3fff24c
Ezequiel Soldano <55820209+ezequielsoldano@users.noreply.github.com>
2023-06-26 11:38:38 -0300

    fix(mate-ui): tabs focus styles

--- libs/mate-ui/src/lib/components/tabs.tsx
+++ libs/mate-ui/src/lib/components/tabs.tsx
@@ -1,17 +1,18 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
 
-import { cn } from "../utils"
+import { cn } from '../utils';
 import { Slot } from '@radix-ui/react-slot';
 
-interface TabsTriggerProps extends React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> {
+interface TabsTriggerProps
+  extends React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> {
   icon?: React.ReactNode;
   notifications?: number;
 }
 
-const Tabs = TabsPrimitive.Root
+const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -28,16 +29,12 @@ const TabsList = React.forwardRef<
         'p-1',
         className
       )}
-      {...props} />
-    <div className={cn(
-      'h-1',
-      'border-b-2',
-      'border-neutral-400',
-      'w-full'
-    )} />
+      {...props}
+    />
+    <div className={cn('h-1', 'border-b-2', 'border-neutral-400', 'w-full')} />
   </>
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
@@ -56,6 +53,7 @@ const TabsTrigger = React.forwardRef<
       'text-sm',
       'font-regular',
       'text-neutral-500',
+      'outline-focus',
       'disabled:pointer-events-none',
       'disabled:opacity-50',
       'data-[state=active]:text-neutral-900',
@@ -67,34 +65,29 @@ const TabsTrigger = React.forwardRef<
     )}
     {...props}
   >
-    {icon &&
-      <Slot className={cn(
-        'h-4',
-        'w-4',
-        'mr-[0.625rem]'
-      )}>
-        {icon}
-      </Slot>}
+    {icon && <Slot className={cn('h-4', 'w-4', 'mr-[0.625rem]')}>{icon}</Slot>}
     {children}
-    {notifications &&
-      <div className={cn(
-        'w-[1.375rem]',
-        'h-[1.563rem]',
-        'rounded-full',
-        'px-2',
-        'py-1',
-        'ml-[0.625rem]',
-        'text-xs',
-        'text-neutral-700',
-        'font-semibold',
-        'bg-neutral-200'
-      )}>
+    {notifications && (
+      <div
+        className={cn(
+          'w-[1.375rem]',
+          'h-[1.563rem]',
+          'rounded-full',
+          'px-2',
+          'py-1',
+          'ml-[0.625rem]',
+          'text-xs',
+          'text-neutral-700',
+          'font-semibold',
+          'bg-neutral-200'
+        )}
+      >
         {notifications}
-      </div>}
-
+      </div>
+    )}
   </TabsPrimitive.Trigger>
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
@@ -113,7 +106,7 @@ const TabsContent = React.forwardRef<
     )}
     {...props}
   />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
