1a7af465fe5c554d48dd4edfe47f32f946f6421a
esosa-tn <96207081+esosa-tn@users.noreply.github.com>
2023-07-26 12:42:56 -0300

    Added favicon (#55)

--- apps/mate-ui-docs/pages/examples/index.tsx
+++ apps/mate-ui-docs/pages/examples/index.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { BaseLayout } from '@/layouts/BaseLayout';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@truenorth/mate-ui';
 import { Dashboard } from '@components';
@@ -16,36 +17,41 @@ const tabs = [
 
 const Page: NextPageWithLayout = () => {
   return (
-    <div className="flex flex-col">
-      <header className="flex w-full">
-        <div className="flex flex-col w-full pb-12">
-          <h3 className="text-3xl font-semibold text-neutral-900 pb-6">
-            Dashboard
-          </h3>
-          <h5 className="text-xl text-neutral-700">
-            Explore some examples of layouts built using Mate UI components
-          </h5>
-        </div>
-      </header>
-      <Tabs defaultValue={tabs[0].name}>
-        <TabsList>
+    <>
+      <Head>
+        <link rel="icon" href="/assets/favicon.png" />
+      </Head>
+      <div className="flex flex-col">
+        <header className="flex w-full">
+          <div className="flex flex-col w-full pb-12">
+            <h3 className="text-3xl font-semibold text-neutral-900 pb-6">
+              Dashboard
+            </h3>
+            <h5 className="text-xl text-neutral-700">
+              Explore some examples of layouts built using Mate UI components
+            </h5>
+          </div>
+        </header>
+        <Tabs defaultValue={tabs[0].name}>
+          <TabsList>
+            {tabs.map((tab) => {
+              return (
+                <TabsTrigger key={tab.key} value={tab.name}>
+                  {tab.name}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
           {tabs.map((tab) => {
             return (
-              <TabsTrigger key={tab.key} value={tab.name}>
-                {tab.name}
-              </TabsTrigger>
+              <TabsContent key={tab.key} value={tab.name}>
+                {tab.component}
+              </TabsContent>
             );
           })}
-        </TabsList>
-        {tabs.map((tab) => {
-          return (
-            <TabsContent key={tab.key} value={tab.name}>
-              {tab.component}
-            </TabsContent>
-          );
-        })}
-      </Tabs>
-    </div>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
